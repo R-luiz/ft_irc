6e03beb2554ce542c0d88c5f1657b2f49fc78485
Robin Luiz <rluiz@c2r3p2.42lehavre.fr>
2024-07-22 13:38:19 +0200

    auto-push

--- Server.hpp
+++ Server.hpp
@@ -3,59 +3,54 @@
 
 #include <string>
 #include <vector>
+#include <map>
+#include <poll.h>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <cerrno>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <poll.h>
-#include "Channel.hpp"
-#include "Message.hpp"
-#include "MessageQueue.hpp"
-
+#include <arpa/inet.h>
 class Client;
 class Channel;
 
 class Server {
 private:
+    int serverSocket;
     int port;
     std::string password;
-    int serverSocket;
     std::vector<pollfd> fds;
-    std::vector<Client*> clients;
-    std::vector<Channel*> channels;
+    std::map<int, Client*> clients;
+    std::map<std::string, Channel*> channels;
+
+    void setup();
+    void handleNewConnection();
+    void handleClientMessage(int clientFd);
+    void removeClient(int clientFd);
+    void processCommand(Client* client, const std::string& message);
+
+    // Mandatory command handlers
+    void handlePass(Client* client, const std::string& params);
+    void handleNick(Client* client, const std::string& params);
+    void handleUser(Client* client, const std::string& params);
+    void handleJoin(Client* client, const std::string& params);
+    void handlePrivmsg(Client* client, const std::string& params);
+    void handleKick(Client* client, const std::string& params);
+    void handleInvite(Client* client, const std::string& params);
+    void handleTopic(Client* client, const std::string& params);
+    void handleMode(Client* client, const std::string& params);
+
+    Channel* getChannel(const std::string& channelName);
+    void broadcastToChannel(const Channel* channel, const std::string& message, Client* exclude = nullptr);
 
 public:
     Server(int port, const std::string& password);
     ~Server();
 
     void run();
-
-private:
-    void setup();
-    void handleNewConnection();
-    void handleClientMessage(int clientFd);
-    void removeClient(int clientFd);
-    void processMessage(int clientFd, const std::string& message);
-	void handleCommand(Client* client, const std::string& command, const std::string& params);
-    void handleNick(Client* client, const std::string& nickname);
-    void handleUser(Client* client, const std::string& params);
-    void handlePing(Client* client, const std::string& token);
-    void authenticateClient(Client* client);
-	Client* getClientByFd(int fd);
-	void handleJoin(Client* client, const std::string& params);
-    void handlePrivmsg(Client* client, const std::string& params);
-    Channel* getChannelByName(const std::string& name);
-	Client* getClientByNickname(const std::string& nickname);
-	void handlePart(Client* client, const std::string& params);
-    void handleKick(Client* client, const std::string& params);
-    void handleInvite(Client* client, const std::string& params);
-    void handleTopic(Client* client, const std::string& params);
-    void handleMode(Client* client, const std::string& params);
-    void handlePass(Client* client, const std::string& params);
-	MessageQueue messageQueue;
-    void processMessageQueue();
-    void queueMessage(Client* recipient, const std::string& content);
-    void broadcastServerMessage(const std::string& message);
-    std::string getServerName() const;
-    std::string getServerCreationDate() const;
 };
 
-#endif
+#endif // SERVER_HPP
