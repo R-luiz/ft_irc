2ab4d1e61cefd7ea3122a4819b8de8689ba5e1e7
Robin Luiz <rluiz@c2r5p2.42lehavre.fr>
2024-08-07 15:38:13 +0200

    repaired awful stuff, added log file, reworked structure with pointer instead of objects

--- Client.cpp
+++ Client.cpp
@@ -1,6 +1,27 @@
 #include "Client.hpp"
 
-Client::Client() {}
+Client::Client() : Fd(-1), authenticated(false), user(new User()) {}
+
+Client::Client(const Client& other)
+    : Fd(other.Fd), IPadd(other.IPadd), 
+      authenticated(other.authenticated),user(new User(*other.user)), buffer(other.buffer) {}
+
+Client::~Client()
+{
+	delete user;
+}
+
+Client& Client::operator=(const Client& other) {
+	if (this != &other) {
+		Fd = other.Fd;
+		IPadd = other.IPadd;
+		delete user;
+		user = new User(*other.user);
+		buffer = other.buffer;
+		authenticated = other.authenticated;
+	}
+	return *this;
+}
 
 void Client::setIpAdd(std::string ipadd)
 {
@@ -30,4 +51,29 @@ void Client::clearBuffer()
 std::string& Client::getBuffer()
 {
 	return buffer;
+}
+
+bool Client::isAuthenticated()
+{
+	return authenticated;
+}
+
+void Client::setAuthenticated(bool auth)
+{
+	authenticated = auth;
+}
+
+User* Client::getUser()
+{
+	return user;
+}
+
+std::string Client::getIPadd()
+{
+	return IPadd;
+}
+
+void Client::setIPadd(std::string ipadd)
+{
+	IPadd = ipadd;
 }
