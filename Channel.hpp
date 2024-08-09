fd3b66f6b2ebf5d91ceb4c6db97afde1e8816b14
Amelie Ramier <aramier@c2r3p1.42lehavre.fr>
2024-08-09 11:06:21 +0200

    synthax

--- Channel.hpp
+++ Channel.hpp
@@ -9,27 +9,28 @@
 
 class User;
 
-class Channel {
-private:
-    std::string name;
-    std::string topic;
-    std::vector<User*> users;
-    std::map<User*, bool> operators; // true if the user is an operator
+class Channel 
+{
+    private:
+        std::string name;
+        std::string topic;
+        std::vector<User*> users;
+        std::map<User*, bool> operators; // true if the user is an operator
 
-public:
-    Channel(const std::string& channelName);
-    ~Channel();
+    public:
+        Channel(const std::string& channelName);
+        ~Channel();
 
-    void addUser(User* user);
-    void removeUser(User* user);
-    bool hasUser(User* user) const;
-    void setTopic(const std::string& newTopic);
-    std::string getTopic() const;
-    std::string getName() const;
-    std::vector<User*> getUsers() const;
-    bool isOperator(User* user) const;
-    void setOperator(User* user, bool status);
-    void broadcastMessage(const std::string& message, User* sender);
+        void addUser(User* user);
+        void removeUser(User* user);
+        bool hasUser(User* user) const;
+        void setTopic(const std::string& newTopic);
+        std::string getTopic() const;
+        std::string getName() const;
+        std::vector<User*> getUsers() const;
+        bool isOperator(User* user) const;
+        void setOperator(User* user, bool status);
+        void broadcastMessage(const std::string& message, User* sender);
 };
 
 #endif
--- User.cpp
+++ User.cpp
@@ -2,49 +2,60 @@
 
 User::User() : nickname(""), username(""), hostname(""), realname(""), fd(0) {}
 
-User::User(std::string nick, std::string user) {
+User::User(std::string nick, std::string user) 
+{
 	nickname = nick;
 	username = user;
 }
 
 User::~User() {}
 
-std::string User::getNick() {
+std::string User::getNick() 
+{
 	return nickname;
 }
 
-std::string User::getUser() {
+std::string User::getUser() 
+{
 	return username;
 }
 
-std::string User::getHostname() {
+std::string User::getHostname() 
+{
 	return hostname;
 }
 
-int User::getFd() {
+int User::getFd() 
+{
 	return fd;
 }
 
-void User::setNick(std::string nick) {
+void User::setNick(std::string nick) 
+{
 	nickname = nick;
 }
 
-void User::setUser(std::string user) {
+void User::setUser(std::string user) 
+{
 	username = user;
 }
 
-void User::setHostname(std::string host) {
+void User::setHostname(std::string host) 
+{
 	hostname = host;
 }
 
-void User::setFd(int fd) {
+void User::setFd(int fd) 
+{
 	this->fd = fd;
 }
 
-void User::setRealName(std::string real) {
+void User::setRealName(std::string real) 
+{
 	realname = real;
 }
 
-std::string User::getRealName() {
+std::string User::getRealName() 
+{
 	return realname;
 }
